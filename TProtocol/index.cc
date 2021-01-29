7d9fb22664b5dc288e2fb1a3f9f23ce66765c5b0
ScSofts <3416616002@qq.com>
2021-01-29 16:39:41 +0800

    Fix small errors.

--- TProtocol/index.cc
+++ TProtocol/index.cc
@@ -7,7 +7,8 @@ int main(int argc, char *argv[]){
     ld::HexString hex = BuildLoginPack(
         NetInt.LittleEidan.u32(3416616002),
         "C3 2A 36 A8 2F 80 6C 6F 76 06 A0 DF 60 71 98 CF"_hex,
-        keyGroup.share_key);
+        keyGroup.share_key,
+        keyGroup.public_key);
     std::cout << hex << std::endl;
     return 0;
 }
